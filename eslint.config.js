03667e6ccd2cbd89ce95642e6936514f14f1d8d8
Johnny Rocket <0.snilcy@gmail.com>
2025-06-29 01:47:49 +0300

    feat: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import 'eslint-plugin-only-warn'
 
+import hooks from './src/hooks.js'
 import modulesNewlines from './src/modules-newlines.js'
 import perfectionistConfig from './src/perfectionist.js'
 import prettier from './src/prettier.js'
@@ -16,4 +17,5 @@ export default [
   ...typescript,
   rules,
   typescriptRules,
+  hooks,
 ]
